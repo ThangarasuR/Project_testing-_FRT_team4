3a17c14c3d48920ad1d2c61e1f1df2e03086ffdf
Prithika <148432277+ThangarasuR@users.noreply.github.com>
2023-12-14 13:53:02 +0530

    Update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path'); // Node.js module for working with file paths
 
 const app = express();
-const port = 3000;
+app.set("port", process.env.PORT || 5000);
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
@@ -12,7 +12,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is listening at http://localhost:${port}`);
-});
+app.listen(app.get("port"), function () {
+    console.log("Node app is running at localhost:" + app.get("port"));
+  });
